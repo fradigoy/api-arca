633a6361fe0c7f21f58e0221b154a709606bbbe6
fredostar <dev@frederic-radigoy.fr>
2020-02-06 22:29:32 +0100

    commit create du projet

---
+++ apiarca/src/main/java/fr/fradigoy/apiarca/ApiarcaApplication.java
@@ -0,0 +1,13 @@
+package fr.fradigoy.apiarca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiarcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiarcaApplication.class, args);
+	}
+
+}
---
+++ apiarca/src/test/java/fr/fradigoy/apiarca/ApiarcaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fradigoy.apiarca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiarcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
